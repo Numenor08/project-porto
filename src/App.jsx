4778bf014c737dc72ae9f3b70edb3dff8d79746c
Numenor08 <athaya120@gmail.com>
2024-06-04 16:03:24 +0700

    feat(ui): add prototype design for topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-function App() {
+import Topbar from "./components/Topbar";
+import "@css/main.css";
 
+function App() {
   return (
     <>
-      
+      <Topbar />
+      <main className="main-container">
+        <h1>React App</h1>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,21 @@
+import "@css/topbar.css";
+
+const Topbar = () => {
+  return (
+    <>
+      <header className="topbar-container">
+        <div>
+          <h2 className="dot">Activities</h2>
+        </div>
+        <div>
+          <h2>Fri 12:35</h2>
+        </div>
+        <div>
+          <h2>Logo</h2>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Topbar;
